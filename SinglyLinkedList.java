9bc0c2fd8fb4c5ec70104928b1eb93bfb3139dc6
Osama A <41015632+codingXllama@users.noreply.github.com>
2019-06-02 22:47:19 -0600

    Created 2 new methods for a linked list

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,48 +1,85 @@
-package DataStructuresAndAlgorithms;
-
 import java.util.List;
 
 public class SinglyLinkedList {
-    // Creating the head of the list (Instance variable)
-    private ListNode head;
+    private ListNode headNode;
 
-    // Creating the Static inner ListNode class
     private static class ListNode {
-        //Creating the data field
-        private int data;
-        //Creating the nextNode reference
+        private int dataField;
         private ListNode nextNode;
 
-        //Creating a constructor that allows us to create new listNodes
+        //Creating the constructor for ListNode - this allows us to create Nodes for our Linked list
         public ListNode(int data) {
-            //Setting the data field for the linked list
-            this.data = data;
-            //the beginning node (head node) of the list always points at null when the Linked list is empty
+            dataField = data;
             this.nextNode = null;
         }
     }
 
+    /*
+        1->3->15->6-->20
+
+
+     */
+
+    public void DisplayLinkedList() {
+//Displaying the SinglyLinked list linkedList elements
+        ListNode currentNode = headNode;
+        while (currentNode != null) {
+            System.out.print(currentNode.dataField + " --> ");
+            currentNode = currentNode.nextNode;
+        }
+        System.out.print("null ");
+    }
+
+    public int LinkedListSize() {
+
+        //checking if the headNode is empty
+        if (headNode==null)
+        {
+            return 0;
+        }
+
+        ListNode currentNode = headNode;
+        int listSizeCounter = 0;
+        while (currentNode != null) {
+            listSizeCounter += 1;
+            currentNode = currentNode.nextNode;
+        }
+        return listSizeCounter;
+    }
+
+
+    public void InsertFront() {
+        ListNode tempNode = headNode;
+    }
+
+
     public static void main(String[] args) {
-        // Once we create the SinglyLinkedList then the head will automatically be null, since there is no other ListNode attach to it
-        SinglyLinkedList mySinglyLinkedList_Object = new SinglyLinkedList();
-        //Creating the headNode
-        mySinglyLinkedList_Object.head = new ListNode(10); // head: 10 --> nextNode: null
-        // Creating the secondNode with a data value of 1
-        ListNode secondNode = new ListNode(1);
-        //Creating the secondNode with a data value of 2
-        ListNode thirdNode = new ListNode(20);
-        ListNode fourthNode = new ListNode(12);
+//      Creating a SinglyLinked list object and nodes for the SinglyLinked list
+        SinglyLinkedList linkListObject = new SinglyLinkedList();
+        SinglyLinkedList newLinkedListObject = new SinglyLinkedList();
+        linkListObject.headNode = new ListNode(1);
+        newLinkedListObject.headNode = new ListNode(44);
+        ListNode firstNode = new ListNode(3);
+        ListNode secondNode = new ListNode(15);
+        ListNode thirdNode = new ListNode(6);
+        ListNode fourthNode = new ListNode(20);
+
+        ListNode newNode = new ListNode(0);
+
+//      Connecting all the nodes together by linking each nextNode field with the dataField
+        linkListObject.headNode.nextNode = firstNode; //1->3
+        firstNode.nextNode = secondNode;//1->3->15
+        secondNode.nextNode = thirdNode;//1->3->15->6
+        thirdNode.nextNode = fourthNode;//1->3->15->6->null
 
 
-        //Connecting all the ListNodes together to form a chain which is a singly Linked list
-        //In order to form a chain we must assign the value(data field) of secondNode the data field of the headNode (mySinglyList_Object)
-        mySinglyLinkedList_Object.head.nextNode=secondNode; // 10 -> 1
-        secondNode.nextNode=thirdNode;  //10-> 1 -> 20
-        thirdNode.nextNode=fourthNode; //10 -> 1 -> 20 -> 12 --> null
+//      Displaying the Singly Linked list by calling the DisplayLinkedList method on the LinkedList object
+        linkListObject.DisplayLinkedList();
 
-        System.out.println(fourthNode);
-
+//       Displaying the size of the linkedList by calling the
+        System.out.println("\nThe linkedlist size is: " + linkListObject.LinkedListSize());
 
     }
+
 
 }
