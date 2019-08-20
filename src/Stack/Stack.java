d0aa64a3907d83ef6ce686fcd8f0483070f874d0
codingxllama <osamaalchalabi2@gmail.com>
2019-08-19 18:47:57 -0600

    Created a stack menu

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -1,5 +1,8 @@
 package Stack;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
 import javax.swing.*;
 import java.util.Scanner;
 
@@ -31,12 +34,13 @@ public class Stack {
     public int getStackSize()
     {
         return stackSize;
+
     }
 
 //    checking if stack is empty
-    public boolean isEmpty()
+   public boolean isEmpty()
     {
-        return stackSize==0;
+        return  stackSize==0;
     }
 
 //    Pushing item onto the stack
@@ -63,13 +67,82 @@ public class Stack {
 
     public static void main (String args[])
     {
-//        Creating a stack object
-        Stack myStack= new Stack();
+
         Scanner userInput= new Scanner(System.in);
-        System.out.print("Enter element to push onto stack: ");
-        int userVal=userInput.nextInt();
-        myStack.Push(userVal);
-        myStack.DisplayStack();
+        boolean menuExit=false;
+
+        Stack myStack= new Stack();
+        //Creating the Stack Menu.
+        System.out.println("<-------- MENU --------->");
+        System.out.println("Press 1 to Push Element");
+        System.out.println("Press 2 to Pop Element");
+        System.out.println("Press 3 to Check Stack Size");
+        System.out.println("Press 4 to Check if Stack is Empty");
+        System.out.println("Press 5 to Display the Stack");
+        System.out.println("Press 6 to Exit Menu\n");
+
+        while(!menuExit)
+        {
+            //taking user Input
+            System.out.print("Your Choice: ");
+            int userOption=userInput.nextInt();
+
+            switch (userOption)
+            {
+                case 1:
+                    System.out.print("Enter element to push onto the stack: ");
+                    int userVal=userInput.nextInt();
+                    myStack.Push(userVal);
+                        break;
+                case 2:
+                case 3:
+                    System.out.println("The Stack size: "+myStack.getStackSize());
+
+                break;
+                case 4:
+                    if (myStack.isEmpty())
+                {
+                    System.out.printf("Stack is Empty\n");
+                }
+                else {
+                    System.out.printf("The Stack is NOT Empty\n");
+                }
+
+                break;
+                case 5:myStack.DisplayStack();break;
+                case 6: menuExit=true; break;
+                default:
+                    System.out.println("Invalid option, please try again!");
+                break;
+            }
+
+        }
+        System.out.println("Have a good life!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        Creating a stack object
+
+
+
+
+
+
+
     }
 
 }
